d943a53a650aed5e89d72e120901d369cd9e9cda
Milan andrei daniel Groen <mgroen@f1r1s18.codam.nl>
2021-12-08 20:23:46 +0100

    exec working

--- minishell/src/exec.c
+++ minishell/src/exec.c
@@ -1,5 +1,34 @@
 #include "../inc/minishell.h"
 
+char	*ft_strjoin_c(char const *s1, char const *s2)
+{
+	char	*new_str;
+	int		i;
+	int		l;
+
+	i = 0;
+	l = 0;
+	if (!s1 || !s2)
+		return (0);
+	new_str = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!new_str)
+		return (0);
+	while (s1[i] != '\0')
+	{
+		new_str[i] = s1[i];
+		i++;
+	}
+	new_str[i] = '/';
+	i++;
+	while (s2[l] != '\0')
+	{
+		new_str[i + l] = s2[l];
+		l++;
+	}
+	new_str[i + l] = '\0';
+	return (new_str);
+}
+
 char	*get_path(char *cmd, char **env)
 {
 	char	*path;
@@ -15,7 +44,7 @@ char	*get_path(char *cmd, char **env)
 	dirs = ft_split(path, ':');
 	while (dirs[i])
 	{
-		cmdfile = ft_strjoin(dirs[i], cmd);
+		cmdfile = ft_strjoin_c(dirs[i], cmd);
 		free(dirs[i]);
 		if (access(cmdfile, F_OK) == 0)
 			break ;
@@ -32,9 +61,19 @@ int exec(char *av, char **env)
 {
     char    **command;
     char    *path;
+	int		id;
 
-    command = ft_split(av, ' ');
+	id = fork();
+	if (id == -1)
+		perror("fork error");
+	if (id)
+	{
+		waitpid(id, NULL, 0);
+		return (0);
+	}
+	command = ft_split(av, ' ');
     path = get_path(command[0], env);
-    execve(path, command, env);
-    perror("error");
+	execve(path, command, env);
+    perror("command error");
+	return (1);
 }
--- minishell/src/minishell.c
+++ minishell/src/minishell.c
@@ -44,11 +44,13 @@ int	ft_find_redirect(t_info *info)
 int	ft_find_command(t_info *info, char **env)
 {
 	if (ft_strncmp(info->line_read, "echo", 4) == 0)
-		return (exec("/echo", env));
+		return (exec(info->line_read, env));
 	if (ft_strncmp(info->line_read, "cd", 2) == 0)
-		return (1);
+		return (exec(info->line_read, env));
 	if (ft_strncmp(info->line_read, "pwd", 3) == 0)
-		return (2);
+		return (exec(info->line_read, env));
+	if (ft_strncmp(info->line_read, "exit", 3) == 0)
+		exit(0);
 	return (15);
 }
 void	parseline(t_info *info, char **av, char **env)
@@ -56,7 +58,7 @@ void	parseline(t_info *info, char **av, char **env)
 		info->lines = ft_split(info->line_read, ' ');
 		info->redirect = ft_find_redirect(info);
 		info->cmd = ft_find_command(info, env);
-		if (info->cmd == command_not_found)
+		if (info->cmd == 15)
 		{
 			printf("minishell: command not found: %s\n", info->line_read);
 			return ;
@@ -68,11 +70,11 @@ int main(int ac, char **av, char **env)
 {
 	t_info info;
 
-	ft_init_struct(&info, av, env);
-	printf("Welcome! You can exit by pressing Ctrl+C at any time...\n");
+	//ft_init_struct(&info, av, env);
+	printf("\033[1;33mWelcome! You can exit by pressing Ctrl+C at any time...\n");
 	while (1 == 1)
 	{
-		info.line_read = readline("minishell: ");
+		info.line_read = readline("\033[0;33mminishell: \033[0m");
 		if (!info.line_read[0])
 			continue ;
 		parseline(&info, av, env);
