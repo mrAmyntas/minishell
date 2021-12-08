38034e1d5a663b08b16a7ee4ae626d820e45ef7a
Milan andrei daniel Groen <mgroen@f1r1s18.codam.nl>
2021-12-08 17:15:54 +0100

    added exec.c

---
+++ minishell/src/exec.c
@@ -0,0 +1,40 @@
+#include "../inc/minishell.h"
+
+char	*get_path(char *cmd, char **env)
+{
+	char	*path;
+	char	**dirs;
+	char	*cmdfile;
+	int		i;
+
+	i = 0;
+	while (env[i] && ft_strncmp(env[i], "PATH=", 5))
+		i++;
+	path = env[i] + 5;
+	i = 0;
+	dirs = ft_split(path, ':');
+	while (dirs[i])
+	{
+		cmdfile = ft_strjoin(dirs[i], cmd);
+		free(dirs[i]);
+		if (access(cmdfile, F_OK) == 0)
+			break ;
+		free(cmdfile);
+		i++;
+	}
+	free(dirs);
+	if (dirs[i])
+		return (cmdfile);
+	return (cmd);
+}
+
+void exec(char **argv, char **env)
+{
+    char    **command;
+    char    *path;
+
+    command = ft_split(argv[1], ' ');
+    path = get_path(command[0], env);
+    execve(path, command, env);
+    perror("error");
+}
