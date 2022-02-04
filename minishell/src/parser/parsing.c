cb3355cd3753152d77b92babdaf8c19d241e4add
Bas yaw Hoitzing <bhoitzin@f1r6s7.codam.nl>
2022-02-04 21:06:16 +0100

    zooi eruit

--- minishell/src/parser/parsing.c
+++ minishell/src/parser/parsing.c
@@ -6,7 +6,7 @@
 /*   By: bhoitzin <bhoitzin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/12/10 11:34:31 by bhoitzin      #+#    #+#                 */
-/*   Updated: 2022/02/04 19:01:04 by bhoitzin      ########   odam.nl         */
+/*   Updated: 2022/02/04 21:05:46 by bhoitzin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,7 @@
 
 // '> a' -> LEAKS
 
+// nog iets met die filenames met spaties? 'test 1'?
 #include "../../inc/minishell.h"
 
 static void	remove_quotes(t_info *info)
