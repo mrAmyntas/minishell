ca70089d1ec1d1160b70bf379a5c24b63034b388
Bas yaw Hoitzing <bhoitzin@f0r3s16.codam.nl>
2022-02-16 18:04:42 +0100

    fix

--- minishell/src/minishell.c
+++ minishell/src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: bhoitzin <bhoitzin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/01/26 13:23:35 by bhoitzin      #+#    #+#                 */
-/*   Updated: 2022/02/16 18:02:22 by mgroen        ########   odam.nl         */
+/*   Updated: 2022/02/16 18:03:46 by bhoitzin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
